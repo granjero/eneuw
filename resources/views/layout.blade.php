24516fc17131567317a605a432225af1f1f8c07f
juan miguel <juanmiguel@synergy.local>
2022-01-19 07:12:47 -0300

    favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/atom-one-light.min.css') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     @yield('estilos')
 </head>
 <body>
